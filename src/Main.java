95b32f3a62b6ef1c7b8d331a70d0c526419c4b5a
6-god <53241447+6-god@users.noreply.github.com>
2021-12-25 00:43:48 +0800

    small changes 2

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,9 @@ public class Main {
 //        TransferResult.getInstance().getJf().setVisible(true);
 //
         //要让哪个frame显示出来的话，就像上面那样写: xxx.getInstance().getJf().setVisible(true);
+        //主要就是把能对齐的对对齐，该居中的居中，字体、界面啥的大小给整合理了
+        //整的好看点，按钮之间还有按钮和界面的边框之间留一点空
+        //变量的名字啥的就别动了，加油，gkd
 
     }
 }
